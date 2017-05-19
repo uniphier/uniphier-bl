uniphier/uniphier-bl

commit a5a05b72cf9266954764ecfb9a2d53408a78d048

Build: fix eternal recursion when O=<path> includes symlinks
Kunihiko Hayashi reports that the Make falls into an endless
recursion if the out-of-tree building path includes symlinks:

  $ mkdir foo
  $ ln -s foo bar
  $ make O=bar
  make[1]: Entering directory `/home/masahiro/uniphier-bl/foo'
  make[2]: Entering directory `/home/masahiro/uniphier-bl/foo'
  make[3]: Entering directory `/home/masahiro/uniphier-bl/foo'
     ...

This can be fixed by replacing $(abspath ...) with $(realpath ...)
because the latter resolves symlinks in contrast to the former.

The addressed problem is actually fixed by the replacement of the
second instance, but I am replacing the first one as well because
$(realpath ...) seems to do a better job than $(abspath ...).

Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
Reported-by: Kunihiko Hayashi <hayashi.kunihiko@socionext.com>

Makefile
@@ -27,14 +27,14 @@ endif
 
 export quiet Q
 
-SRCDIR := $(abspath $(dir $(lastword $(MAKEFILE_LIST))))
+SRCDIR := $(realpath $(dir $(lastword $(MAKEFILE_LIST))))
 
 ifeq ("$(origin O)", "command line")
   UNPH_OUTPUT := $O
 endif
 
 ifneq ($(UNPH_OUTPUT),)
-UNPH_OUTPUT := $(abspath $(UNPH_OUTPUT))
+UNPH_OUTPUT := $(realpath $(UNPH_OUTPUT))
 endif
 
 ifneq ($(filter-out $(CURDIR),$(UNPH_OUTPUT)),)

commit 5f03a0c166d1a12c5bcac2c79140d2a706767b62

Build: allow O=<path> to point to non-existing directory
Since commit a5a05b72cf92 ("Build: fix eternal recursion when O=<path>
includes symlinks"), the output directory is not automatically created
because $(realpath ...) returns an empty string for a non-existing
path input.

Fixes: a5a05b72cf92 ("Build: fix eternal recursion when O=<path> includes symlinks")
Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>

Makefile
@@ -40,12 +40,13 @@ ifeq ("$(origin O)", "command line")
 endif
 
 ifneq ($(UNPH_OUTPUT),)
+saved-output := $(UNPH_OUTPUT)
+$(shell mkdir -p $(UNPH_OUTPUT))
 UNPH_OUTPUT := $(realpath $(UNPH_OUTPUT))
+$(if $(UNPH_OUTPUT),,$(error cannot create output directory "$(saved-output)"))
 endif
 
 ifneq ($(filter-out $(CURDIR),$(UNPH_OUTPUT)),)
-
-$(shell mkdir -p $(UNPH_OUTPUT))
 
 PHONY += $(MAKECMDGOALS)
 $(filter-out all, $(MAKECMDGOALS)): all