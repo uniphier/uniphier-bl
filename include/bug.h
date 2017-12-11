e3d1eec4da6ab0626226f6f3bd6a482c54dfce6b
Masahiro Yamada <yamada.masahiro@socionext.com>
2017-12-11 17:21:39 +0900

    bug.h: add BUG(), BUG_ON(), WARN(), and WARN_ON_ONCE()
    Mostly imported from include/asm-generic/bug.h, Linux 4.14.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>

--- include/bug.h
+++ include/bug.h
@@ -8,14 +8,43 @@
 #define __BUG_H__
 
 #include <compiler.h>
+#include <printk.h>
+#include <types.h>
 
-#define __WARN()
+#define BUG() do { \
+	printk("BUG at %s():%d!\n", __func__, __LINE__); \
+	do {} while (1); \
+} while (0)
+
+#define BUG_ON(condition) do { if (unlikely(condition)) BUG(); } while (0)
+
+#define __WARN()	printk("WARNING at %s():%d!\n", __func__, __LINE__)
+
+#define __WARN_printf(arg...)	do { printk(arg); __WARN(); } while (0)
+
+#define WARN(condition, format...) ({					\
+	int __ret_warn_on = !!(condition);				\
+	if (unlikely(__ret_warn_on))					\
+		__WARN_printf(format);					\
+	unlikely(__ret_warn_on);					\
+})
 
 #define WARN_ON(condition) ({						\
 	int __ret_warn_on = !!(condition);				\
 	if (unlikely(__ret_warn_on))					\
 		__WARN();						\
 	unlikely(__ret_warn_on);					\
+})
+
+#define WARN_ON_ONCE(condition)	({					\
+	static bool __warned;						\
+	int __ret_warn_once = !!(condition);				\
+									\
+	if (unlikely(__ret_warn_once && !__warned)) {			\
+		__warned = true;					\
+		WARN_ON(1);						\
+	}								\
+	unlikely(__ret_warn_once);					\
 })
 
 #endif /* __BUG_H__ */
