fbf4053ff82fe968874ac4f4cc0fa583ea21e2e8
Masahiro Yamada <yamada.masahiro@socionext.com>
2017-12-11 14:52:46 +0900

    stringify.h: import from Linux 4.14
    This will be needed for sysreg.h.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>

---
+++ include/stringify.h
@@ -0,0 +1,19 @@
+/*
+ * Copied from include/linux/stringify.h, Linux 4.14
+ *
+ * SPDX-License-Identifier:	GPL-2.0
+ */
+
+#ifndef __STRINGIFY_H__
+#define __STRINGIFY_H__
+
+/*
+ * Indirect stringification.  Doing two levels allows the parameter to be a
+ * macro itself.  For example, compile with -DFOO=bar, __stringify(FOO)
+ * converts to "bar".
+ */
+
+#define __stringify_1(x...)	#x
+#define __stringify(x...)	__stringify_1(x)
+
+#endif	/* __STRINGIFY_H__ */
