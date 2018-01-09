uniphier/uniphier-bl

commit de0c1c2dcb891636992d0741efc9609363e0c6ec

malloc: simple: add simple malloc implementation
The malloc()/free() will be needed to replace the large zlib workspace
(>9KB) allocated in stack.

Implement simple, stupid malloc() function - it gets memory from sbrk(),
but never frees it.  This should be enough for our purpose.

Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>

lib/malloc/simple.c
@@ -1,0 +1,78 @@
+/*
+ * Copyright (C) 2017 Socionext Inc.
+ *   Author: Masahiro Yamada <yamada.masahiro@socionext.com>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#include <compiler.h>
+#include <heap.h>
+#include <malloc.h>
+#include <string.h>
+#include <utils.h>
+
+/* memory allocated by malloc() is aligned for any built-in type */
+#define MALLOC_ALIGNMENT	(sizeof(void *))
+
+#define ZERO_SIZE_PTR		((void *)1)
+#define ZERO_OR_NULL_PTR(x)	((x) <= (ZERO_SIZE_PTR))
+
+void *memalign(size_t alignment, size_t size)
+{
+	void *brk, *p;
+
+	if (unlikely(!size))
+		return ZERO_SIZE_PTR;
+
+	brk = sbrk(0);
+
+	/*
+	 * REVISIT:
+	 * 'alignment' must be a power of two.
+	 *  If not, should it be rounded up to the nearest power of two?
+	 */
+	size += PTR_ALIGN(brk, alignment) - brk;
+
+	p = sbrk(size);
+	if (unlikely(p == (void *)-1))
+		return NULL;
+
+	return p;
+}
+
+void *malloc(size_t size)
+{
+	return memalign(MALLOC_ALIGNMENT, size);
+}
+
+void free(void *ptr)
+{
+}
+
+void *calloc(size_t nmemb, size_t size)
+{
+	void *p;
+
+	size *= nmemb;
+
+	p = malloc(size);
+	if (likely(!ZERO_OR_NULL_PTR(p)))
+		memset(p, 0, size);
+
+	return p;
+}
+
+void *realloc(void *ptr, size_t size)
+{
+	void *new;
+
+	/* always move the memory */
+	new = malloc(size);
+
+	if (likely(new && !ZERO_OR_NULL_PTR(ptr))) {
+		memcpy(new, ptr, size);
+		free(ptr);
+	}
+
+	return new;
+}

config.mk
@@ -15,6 +15,9 @@ CONFIG_UART_BAUDRATE=115200
 CONFIG_SOC_LD11=y
 CONFIG_SOC_LD20=y
 
+# choose malloc implementation
+#CONFIG_MALLOC_SIMPLE=y
+
 # no need to change
 CONFIG_TEXT_BASE=0x30000000
 CONFIG_BSS_BASE=0x30010000

include/malloc.h
@@ -1,0 +1,16 @@
+/*
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __MALLOC_H__
+#define __MALLOC_H__
+
+#include <types.h>
+
+void *memalign(size_t alignment, size_t size);
+void *malloc(size_t size);
+void free(void *ptr);
+void *calloc(size_t nmemb, size_t size);
+void *realloc(void *ptr, size_t size);
+
+#endif /* __MALLOC_H__ */

lib/Makefile
@@ -1,2 +1,3 @@
+obj-y	+= malloc/
 obj-y	+= string.o
 obj-y	+= zlib/

lib/malloc/Makefile
@@ -1,0 +1,2 @@
+# stupid implemenation (memory is never freed)
+obj-$(CONFIG_MALLOC_SIMPLE)	+= simple.o

commit 948c2e099255ac4a5bff9bc7d2344b78af038dbb

malloc: simple: fix alignment for allocated memory
The pointer returned from memalign() is not aligned.

Fixes: de0c1c2dcb89 ("malloc: simple: add simple malloc implementation")
Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>

lib/malloc/simple.c
@@ -20,6 +20,7 @@
 void *memalign(size_t alignment, size_t size)
 {
 	void *brk, *p;
+	ptrdiff_t offset;
 
 	if (unlikely(!size))
 		return ZERO_SIZE_PTR;
@@ -31,13 +32,14 @@ void *memalign(size_t alignment, size_t size)
 	 * 'alignment' must be a power of two.
 	 *  If not, should it be rounded up to the nearest power of two?
 	 */
-	size += PTR_ALIGN(brk, alignment) - brk;
+	offset = PTR_ALIGN(brk, alignment) - brk;
+	size += offset;
 
 	p = sbrk(size);
 	if (unlikely(p == (void *)-1))
 		return NULL;
 
-	return p;
+	return p + offset;
 }
 
 void *malloc(size_t size)