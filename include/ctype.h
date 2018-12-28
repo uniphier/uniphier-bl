a51927a1f3cecf415f872467fca5e6e820a6247f
Masahiro Yamada <yamada.masahiro@socionext.com>
2018-12-28 13:36:01 +0900

    ctype: import isdigit() from Linux 4.20
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>

---
+++ include/ctype.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Based on include/linux/ctype.h, Linux 4.20
+ */
+#ifndef CTYPE_H
+#define CTYPE_H
+
+static inline int isdigit(int c)
+{
+	return '0' <= c && c <= '9';
+}
+
+#endif /* CTYPE_H */
