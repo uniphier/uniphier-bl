4fb9054b2b28de4cfb6a08ebf3e707de2ad1a38a
Masahiro Yamada <yamada.masahiro@socionext.com>
2018-12-28 13:37:22 +0900

    printk: use vsnprintf() for the format conversion
    vsnprintf() provides more format specifiers with more precise parsing.
    For example, the field width specifier is useful.
    
    However, this increases the memory footprint by more than 1KB.
    The later commit will disable some specifiers to mitigate it.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>

--- common/printk.c
+++ common/printk.c
@@ -4,6 +4,7 @@
 //   Author: Masahiro Yamada <yamada.masahiro@socionext.com>
 
 #include <printk.h>
+#include <sprintf.h>
 #include <stdarg.h>
 #include <uart.h>
 
@@ -22,100 +23,13 @@ static void __puts(const char *s)
 		__putc(c);
 }
 
-#define get_num_va_args(args, lcount) \
-	((lcount) > 1 ? va_arg(args, long long) :	\
-	 ((lcount) ? va_arg(args, long) : va_arg(args, int)))
-
-#define get_unum_va_args(args, lcount) \
-	((lcount) > 1 ? va_arg(args, unsigned long long) :	\
-	 ((lcount) ? va_arg(args, unsigned long) : va_arg(args, unsigned int)))
-
-static void unsigned_num_print(unsigned long long unum, unsigned int radix)
-{
-	unsigned char buf[32];
-	int i = 0, rem;
-
-	do {
-		rem = unum % radix;
-		buf[i++] = rem > 9 ? rem - 0xa + 'a' : rem + '0';
-	} while (unum /= radix);
-
-	while (--i >= 0)
-		__putc(buf[i]);
-}
-
-static void vprintk(const char *fmt, va_list args)
-{
-	int l_count;
-	long long num;
-	unsigned long long unum;
-	char *str;
-	char c;
-
-	while ((c = *fmt++)) {
-		l_count = 0;
-
-		if (c != '%') {
-			__putc(c);
-			continue;
-		}
-
-		c = *fmt++;
-
-		while (c == 'l') {
-			l_count++;
-			c = *fmt++;
-		}
-
-		if (c == 'z')
-			l_count = 1;
-
-		/* Check the format specifier */
-		switch (c) {
-		case '%':
-			__putc('%');
-			break;
-		case 'i':
-		case 'd':
-			num = get_num_va_args(args, l_count);
-			if (num < 0) {
-				__putc('-');
-				unum = -num;
-			} else
-				unum = num;
-
-			unsigned_num_print(unum, 10);
-			break;
-		case 's':
-			str = va_arg(args, char *);
-			__puts(str);
-			break;
-		case 'p':
-			unum = (unsigned long)va_arg(args, void *);
-			if (unum)
-				__puts("0x");
-
-			unsigned_num_print(unum, 16);
-			break;
-		case 'x':
-			unum = get_unum_va_args(args, l_count);
-			unsigned_num_print(unum, 16);
-			break;
-		case 'u':
-			unum = get_unum_va_args(args, l_count);
-			unsigned_num_print(unum, 10);
-			break;
-		default:
-			break;
-		}
-	}
-}
-
 void printk(const char *fmt, ...)
 {
 	va_list args;
+	char buf[128];
 
 	va_start(args, fmt);
-	vprintk(fmt, args);
+	vsnprintf(buf, sizeof(buf), fmt, args);
+	__puts(buf);
 	va_end(args);
 }
