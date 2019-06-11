26aaf4145b4c37bd11018dc0d713b3be95121326
Masahiro Yamada <yamada.masahiro@socionext.com>
2019-06-11 19:44:02 +0900

    soc-info: use FIELD_GET() for SG_REVISION_TYPE_MASK
    It is tedious to define the mask and shift pair.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>

--- common/soc-info.c
+++ common/soc-info.c
@@ -3,16 +3,17 @@
 // Copyright (C) 2017 Socionext Inc.
 //   Author: Masahiro Yamada <yamada.masahiro@socionext.com>
 
+#include <bits.h>
+#include <bitfield.h>
 #include <io.h>
 #include <soc-info.h>
 
 #define SG_REVISION		(IOMEM(0x5f800000))
-#define   SG_REVISION_TYPE_SHIFT	16
-#define   SG_REVISION_TYPE_MASK		(0xff << SG_REVISION_TYPE_SHIFT)
+#define   SG_REVISION_TYPE_MASK		GENMASK(23, 16)
 
 unsigned int get_soc_id(void)
 {
 	u32 rev = readl(SG_REVISION);
 
-	return (rev & SG_REVISION_TYPE_MASK) >> SG_REVISION_TYPE_SHIFT;
+	return FIELD_GET(SG_REVISION_TYPE_MASK, rev);
 }
